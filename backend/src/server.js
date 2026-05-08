b8e79c4f95ff0130df613da44b91ef01c1bf4645
Jiano Freo <jianofreo.magtangob@tup.edu.ph>
2026-05-08 17:11:11 +0800

    the set up is done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import { ENV } from './config/env.js';
 
 const app = express();
 
 app.get('/api/test',(res, req) =>{
     res.status(200).json({ message: 'Server is healthy' });
-} )
+});
+
+app.listen(ENV.PORT, () => {
+    console.log(`the server is running at ${ENV.PORT}`);
+    console.log(`The env is on ${ENV.NODE_ENV}`)
+});
